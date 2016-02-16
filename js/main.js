96bbaffdb4a75435aa58be4ebc3ecc96b9592671
hypnosdesign <hypnos.design@gmail.com>
2016-02-16 14:32:43 +0100

    init js code and a bunch of css styles

--- js/main.js
+++ js/main.js
@@ -1,3 +1,32 @@
-// my custom jquery plugin
+  /* Variabili (Variables)
+  ***********************************************/
 
-/* my JavaScript */
+  var report = " ";
+  
+
+
+  /* Funzioni (Functions)
+  ***********************************************/
+
+  function print(message) {
+    var doc = document.getElementById("gallery");
+    doc.innerHTML = message;
+  }
+
+  function getFigure() {
+    report += "<figure class='images'>";
+    report += '<a href="'+ photo.image +'"><img class="thumb" src="'+ photo.thumb +'" alt="'+ photo.alt_tag +'">';
+    report += '<figcaption><span>'+ photo.title +'</span>'+ photo.caption +'</figcaption></a>';
+    report += "</figure>"; 
+    return report;
+  }
+
+
+  /* Cicli (Loops) 
+  ***********************************************/
+
+  for (var i = 0; i < photos.length; i++) {
+    photo = photos[i];
+    messages = getFigure(photo);
+    print(messages);
+  } // fine ciclo for (End for cicle)
---
+++ js/photos.js
@@ -0,0 +1,77 @@
+var photos = [
+
+	{   image 	: "img/01.jpg",
+		thumb 	: "img/thumbs/01.jpg",
+		title 	: "Hay Bales",
+		caption : "I love hay bales. Took this snap on a drive through the countryside past some straw fields.",
+		alt_tag : "Hay Bales"
+	},
+	{   image 	: "img/02.jpg",
+		thumb 	: "img/thumbs/02.jpg",
+		title 	: "Lake",
+		caption : " The lake was so calm today. We had a great view of the snow on the mountains from here.",
+		alt_tag : "image of Lake"
+	},
+	{   image 	: "img/03.jpg",
+		thumb 	: "img/thumbs/03.jpg",
+		title 	: "Canyon",
+		caption : "I hiked to the top of the mountain and got this picture of the canyon and trees below.",
+		alt_tag : "image of Canyon"
+	},
+	{   image 	: "img/04.jpg",
+		thumb 	: "img/thumbs/04.jpg",
+		title 	: "Iceberg",
+		caption : "It was amazing to see an iceberg up close, it was so cold but didn’t snow today.",
+		alt_tag : "image of Iceberg"
+	},
+	{   image 	: "img/05.jpg",
+		thumb 	: "img/thumbs/05.jpg",
+		title 	: "Desert",
+		caption : " The red cliffs were beautiful. It was really hot in the desert but we did a lot of walking through the canyons.",
+		alt_tag : "image of Desert"
+	},
+	{   image 	: "img/06.jpg",
+		thumb 	: "img/thumbs/06.jpg",
+		title 	: "Fall",
+		caption : " Fall is coming, I love when the leaves on the trees start to change color.",
+		alt_tag : "image of Fall"
+	},
+	{   image 	: "img/07.jpg",
+		thumb 	: "img/thumbs/07.jpg",
+		title 	: "Plantation",
+		caption : "I drove past this plantation yesterday, everything is so green!",
+		alt_tag : "image of Plantation"
+	},
+	{   image 	: "img/08.jpg",
+		thumb 	: "img/thumbs/08.jpg",
+		title 	: "Dunes",
+		caption : " My summer vacation to the Oregon Coast. I love the sandy dunes!",
+		alt_tag : "image of Dunes"
+	},
+	{   image 	: "img/09.jpg",
+		thumb 	: "img/thumbs/09.jpg",
+		title 	: "Countryside Lane",
+		caption : "We enjoyed a quiet stroll down this countryside lane.",
+		alt_tag : "image of Countryside Lane"
+	},
+	{   image 	: "img/10.jpg",
+		thumb 	: "img/thumbs/10.jpg",
+		title 	: "Sunset",
+		caption : "Sunset at the coast! The sky turned a lovely shade of orange.",
+		alt_tag : "image of Sunset"
+	},
+	{   image 	: "img/11.jpg",
+		thumb 	: "img/thumbs/11.jpg",
+		title 	: "Cave",
+		caption : " I did a tour of a cave today and the view of the landscape below was breathtaking.",
+		alt_tag : "image of Cave"
+	},
+	{   image 	: "img/12.jpg",
+		thumb 	: "img/thumbs/12.jpg",
+		title 	: "Bluebells",
+		caption : " I walked through this meadow of bluebells and got a good view of the snow on the mountain before the fog came in.",
+		alt_tag : "image of Bluebells"
+	}
+
+];
+
