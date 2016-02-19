86a6e43e63ddfcb063c1008c7bc452e32963abec
hypnosdesign <hypnos.design@gmail.com>
2016-02-19 20:18:02 +0100

    add gallery overlay

--- js/main.js
+++ js/main.js
@@ -1,34 +1,102 @@
-  /* Variabili (Variables)
-  ***********************************************/
-
-  var report = " ";
-  
+/***************************
+  JAVASCRIPT
+***************************/
 
 
-  /* Funzioni (Functions)
-  ***********************************************/
+/* Variabili (Variables)
+***********************************************/
 
-  // print a message and append it in the gallery id.
-  function print(message) {
-    var doc = document.getElementById("gallery");
-    doc.innerHTML = message;
-  }
-
-  // return html of the thumbnail
-  function getFigure() {
-    report += "<figure class='images'>";
-    report += '<a href="'+ photo.image +'"><img class="thumb" src="'+ photo.thumb +'" alt="'+ photo.alt_tag +'">';
-    report += '<figcaption><span>'+ photo.title +'</span>'+ photo.caption +'</figcaption></a>';
-    report += "</figure>"; 
-    return report;
-  }
+var report = " ";
 
 
-  /* Cicli (Loops) 
-  ***********************************************/
 
-  // show images in the page
-  for (var i = 0; i < photos.length; i++) {
-    photo = photos[i];
-    print(getFigure(photo));
-  } // fine ciclo for (End for cicle)
+/* Funzioni (Functions)
+***********************************************/
+
+// print a message and append it in the gallery id.
+function print(message) {
+  var doc = document.getElementById("gallery");
+  doc.innerHTML = message;
+}
+
+// return html of the thumbnail
+function getFigure() {
+  report += "<figure class='images'>";
+  report += '<a href="'+ photo.image +'"><img class="thumb" src="'+ photo.thumb +'" alt="'+ photo.alt_tag +'">';
+  report += '<figcaption><span>'+ photo.title +'</span>'+ photo.caption +'</figcaption></a>';
+  report += "</figure>"; 
+  return report;
+}
+
+
+/* Cicli (Loops) 
+***********************************************/
+
+// show images in the page
+for (var i = 0; i < photos.length; i++) {
+  photo = photos[i];
+  print(getFigure(photo));
+} // fine ciclo for (End for cicle)
+
+
+/***************************
+  jQuery
+***************************/
+var $overLay = $("<div class='overlay'></div>");
+var $photo = $("<img>");
+
+
+
+//append overlay
+$("body").append($overLay);
+$overLay.append($photo);
+$overLay.hide();
+
+//click image and show overlay
+$(".images a").click(function(e){
+  e.preventDefault();
+  var $location = $(this).attr("href");
+
+  $overLay.show();
+  $photo.attr("src", $location);
+
+  console.log(this);
+  console.log($location);
+});
+
+// click and hide overlay
+$($overLay).click(function(){
+  $overLay.hide();
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
