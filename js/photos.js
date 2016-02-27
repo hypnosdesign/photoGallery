428323d9bbeca050c0bb9115e7fe682d51572d10
hypnosdesign <hypnos.design@gmail.com>
2016-02-27 18:17:44 +0100

    init test video

--- js/photos.js
+++ js/photos.js
@@ -1,65 +1,70 @@
 var photos = [
 
 	{   image 	: "img/01.jpg",
-		thumb 	: "img/thumbs/01.jpg",
-		title 	: "Hay Bales",
-		caption : "I love hay bales. Took this snap on a drive through the countryside past some straw fields." 
+			thumb 	: "img/thumbs/01.jpg",
+			title 	: "Hay Bales",
+			caption : "I love hay bales. Took this snap on a drive through the countryside past some straw fields." 
 	},
 	{   image 	: "img/02.jpg",
-		thumb 	: "img/thumbs/02.jpg",
-		title 	: "Lake",
-		caption : " The lake was so calm today. We had a great view of the snow on the mountains from here."
+			thumb 	: "img/thumbs/02.jpg",
+			title 	: "Lake",
+			caption : " The lake was so calm today. We had a great view of the snow on the mountains from here."
 	},
 	{   image 	: "img/03.jpg",
-		thumb 	: "img/thumbs/03.jpg",
-		title 	: "Canyon",
-		caption : "I hiked to the top of the mountain and got this picture of the canyon and trees below."
+			thumb 	: "img/thumbs/03.jpg",
+			title 	: "Canyon",
+			caption : "I hiked to the top of the mountain and got this picture of the canyon and trees below."
 	},
 	{   image 	: "img/04.jpg",
-		thumb 	: "img/thumbs/04.jpg",
-		title 	: "Iceberg",
-		caption : "It was amazing to see an iceberg up close, it was so cold but didn’t snow today."
+			thumb 	: "img/thumbs/04.jpg",
+			title 	: "Iceberg",
+			caption : "It was amazing to see an iceberg up close, it was so cold but didn’t snow today."
 	},
 	{   image 	: "img/05.jpg",
-		thumb 	: "img/thumbs/05.jpg",
-		title 	: "Desert",
-		caption : " The red cliffs were beautiful. It was really hot in the desert but we did a lot of walking through the canyons."
+			thumb 	: "img/thumbs/05.jpg",
+			title 	: "Desert",
+			caption : " The red cliffs were beautiful. It was really hot in the desert but we did a lot of walking through the canyons."
 	},
 	{   image 	: "img/06.jpg",
-		thumb 	: "img/thumbs/06.jpg",
-		title 	: "Fall",
-		caption : " Fall is coming, I love when the leaves on the trees start to change color."
+			thumb 	: "img/thumbs/06.jpg",
+			title 	: "Fall",
+			caption : " Fall is coming, I love when the leaves on the trees start to change color."
 	},
 	{   image 	: "img/07.jpg",
-		thumb 	: "img/thumbs/07.jpg",
-		title 	: "Plantation",
-		caption : "I drove past this plantation yesterday, everything is so green!"
+			thumb 	: "img/thumbs/07.jpg",
+			title 	: "Plantation",
+			caption : "I drove past this plantation yesterday, everything is so green!"
 	},
 	{   image 	: "img/08.jpg",
-		thumb 	: "img/thumbs/08.jpg",
-		title 	: "Dunes",
-		caption : " My summer vacation to the Oregon Coast. I love the sandy dunes!"
+			thumb 	: "img/thumbs/08.jpg",
+			title 	: "Dunes",
+			caption : " My summer vacation to the Oregon Coast. I love the sandy dunes!"
 	},
 	{   image 	: "img/09.jpg",
-		thumb 	: "img/thumbs/09.jpg",
-		title 	: "Countryside Lane",
-		caption : "We enjoyed a quiet stroll down this countryside lane."
+			thumb 	: "img/thumbs/09.jpg",
+			title 	: "Countryside Lane",
+			caption : "We enjoyed a quiet stroll down this countryside lane."
 	},
 	{   image 	: "img/10.jpg",
-		thumb 	: "img/thumbs/10.jpg",
-		title 	: "Sunset",
-		caption : "Sunset at the coast! The sky turned a lovely shade of orange."
+			thumb 	: "img/thumbs/10.jpg",
+			title 	: "Sunset",
+			caption : "Sunset at the coast! The sky turned a lovely shade of orange."
 	},
 	{   image 	: "img/11.jpg",
-		thumb 	: "img/thumbs/11.jpg",
-		title 	: "Cave",
-		caption : " I did a tour of a cave today and the view of the landscape below was breathtaking."
+			thumb 	: "img/thumbs/11.jpg",
+			title 	: "Cave",
+			caption : " I did a tour of a cave today and the view of the landscape below was breathtaking."
 	},
 	{   image 	: "img/12.jpg",
-		thumb 	: "img/thumbs/12.jpg",
-		title 	: "Bluebells",
-		caption : " I walked through this meadow of bluebells and got a good view of the snow on the mountain before the fog came in."
-		}
+			thumb 	: "img/thumbs/12.jpg",
+			title 	: "Bluebells",
+			caption : " I walked through this meadow of bluebells and got a good view of the snow on the mountain before the fog came in."
+	},
+	{   video 	: "https://www.youtube.com/embed/GBDY3L6GDPA",
+			poster 	: "img/thumbs/12.jpg",
+			title 	: "Video Test",
+			caption : "Video caption test. "
+	}
 
 ];
 
